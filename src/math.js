09239f292e3cf8a43f7625c20e38268763a857f8
Debdutta <m-debdutta@users.noreply.github.com>
2023-04-12 12:51:13 +0530

    Bug fixed for math.js file.

--- src/math.js
+++ src/math.js
@@ -1,23 +1,30 @@
 const add = function(a, b) {
-	return a + b;
+  return a + b;
 }
 
 const multiply = function(a, b) {
-	return a * b;
+  return a * b;
 }
 
 const subtract = function(a, b) {
-	return a - b;
+  return a - b;
 }
 
 const divide = function(a, b) {
-	return a % b;
+  return a % b;
 }
 
 const isEven = function(a) {
-	return a % 2 == 0;
+  return a % 2 == 0;
 }
 
 const isOdd = function(a) {
-	return !isEven(a);
+  return !isEven(a);
 }
+
+exports.isEven = isEven;
+exports.isOdd = isOdd;
+exports.divide = divide;
+exports.multiply = multiply;
+exports.add = add;
+exports.subtract = subtract;
--- test/math-test.js
+++ test/math-test.js
@@ -1,7 +1,10 @@
+const math = require('../src/math.js');
+const test = require('../lib/testing.js');
+
 const testArithematicOperations = function() {
-	test.assertEquality(2, add(1, 1), "1 + 1 should be 2");
-	test.assertEquality(3, subtract(4, 1), "4 - 1 should be 3");
-	test.assertEquality(6, multiply(3, 2), "3 * 2 should be 6");
+  test.assertEquality(2, math.add(1, 1), "1 + 1 should be 2");
+  test.assertEquality(3, math.subtract(4, 1), "4 - 1 should be 3");
+  test.assertEquality(6, math.multiply(3, 2), "3 * 2 should be 6");
 }
 
 testArithematicOperations();
