7b138ddaea28e4975ed62385f033f600d4ed45b8
Debdutta <m-debdutta@users.noreply.github.com>
2023-04-12 13:09:00 +0530

    Bug fixed : array.js is working.

--- src/arrays.js
+++ src/arrays.js
@@ -1,20 +1,25 @@
+const math = require('../src/math.js');
+
 const selectEvenNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isEven(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (math.isEven(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
 const selectOddNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isOdd(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (math.isOdd(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
+
+exports.selectOddNumbers = selectOddNumbers;
+exports.selectEvenNumbers = selectEvenNumbers;
